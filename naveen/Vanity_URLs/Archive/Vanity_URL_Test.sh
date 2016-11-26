742fd8f2304ced4bb9c0b74295664dee342c010b
nnunna <nnunna81@gmail.com>
2016-11-26 16:52:57 -0600

    Deleted naveen forlder

--- naveen/Vanity_URLs/Archive/Vanity_URL_Test.sh
+++
@@ -1,7 +0,0 @@
-for server in lenldtwbdv1d01 lenldtwbdm1d01 lenldtwbte1t01 lenldtwbte2t01 colldtwbte3t01 colldtwblt1l01 colldtwblt1l02 lenldtwblt2l01 lenldtwblt2l02;
-do    
-	echo -e '\n\n #*************************************** START *****************************************#';    
-	echo -e '\n'$i'. Server: ' $server '\n\a';    
-	ssh $server chef-client;    
-	echo -e '\n\n #**************************************** END ******************************************#';  
-done
